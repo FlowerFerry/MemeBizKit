33b4b7bd6f8e7960e415ad0be1707232c4919a81
FlowerFerry <422040095@qq.com>
2024-04-27 03:23:28 +0800

    -

--- include/mmbkpp/wrap/uvw/tcp.h
+++ include/mmbkpp/wrap/uvw/tcp.h
@@ -12,4 +12,40 @@ namespace uvw {
 }
 #endif
 
+namespace mmbkpp {
+    inline int uvw_bind_ipv4only(uvw::TCPHandle& _hdl, const std::string& _host, unsigned _port)
+    {
+#ifndef MMBKPP_WRAP_UVW_3_0_AFTER_DISABLED        
+        sockaddr_storage storage;
+        memset(&storage, 0, sizeof(storage));
+        if (uv_ip4_addr(_host.data(), _port, (sockaddr_in*)&storage) == 0)
+        {
+        }
+        else if (uv_ip6_addr(_host.data(), _port, (sockaddr_in6*)&storage) == 0)
+        {
+        }
+        return _hdl.bind(*((sockaddr*)&storage));
+#else
+        return _hdl.bind<uvw::IPv4>(_host, _port);
+#endif
+    }
+
+    inline int uvw_bind(uvw::TCPHandle& _hdl, const std::string& _host, unsigned _port)
+    {
+#ifndef MMBKPP_WRAP_UVW_3_0_AFTER_DISABLED
+        sockaddr_storage storage;
+        memset(&storage, 0, sizeof(storage));
+        if (uv_ip4_addr(_host.data(), _port, (sockaddr_in*)&storage) == 0) 
+        {
+        }
+        else if (uv_ip6_addr(_host.data(), _port, (sockaddr_in6*)&storage) == 0) 
+        {
+        }
+        return _hdl.bind(*((sockaddr*)&storage));
+#else
+        return _hdl.bind<uvw::IPv6>(_host, _port);
+#endif
+    }
+}
+
 #endif // !MMBKPP_WRAP_UVW_TCP_H_INCLUDED
