a1202aa11e4a1c00f7b94560a0f1b4d52b368e5d
FlowerFerry <422040095@qq.com>
2024-07-08 15:07:27 +0800

    -

---
+++ unit_test/obf_readdir/main.cpp
@@ -0,0 +1,31 @@
+﻿
+#include <mmbkpp/obf/hardware/mmc.h>
+#include <memepp/convert/fmt.hpp>
+#include <fmt/format.h>
+
+int main()
+{
+    std::vector<mmbkpp::obfhw::mmc_info> mmcs;
+    auto e = mmbkpp::obfhw::enum_mmcs(std::back_inserter(mmcs));
+
+    if (e) {
+        fmt::print("[Error][{}] {}\n", e.code(), e.message());
+    }
+
+    for (const auto& mmc : mmcs) {
+        fmt::print("---------------------------------------\n");
+        fmt::print("MMC: {}\n", mmc.dev_name);
+        fmt::print("  Name: {}\n", mmc.name);
+        fmt::print("  CID: {}\n", mmc.cid);
+        fmt::print("  CSD: {}\n", mmc.csd);
+        fmt::print("  OEMID: {}\n", mmc.oemid);
+        fmt::print("  Serial: {}\n", mmc.serial);
+        fmt::print("  Manfid: {}\n", mmc.manfid);
+        fmt::print("  Date: {}\n", mmc.date);
+        fmt::print("  Type: {}\n", mmc.type);
+        fmt::print("  Removable: {}\n", static_cast<int>(mmc.removable));
+    }
+    fmt::print("---------------------------------------\n");
+
+    return 0;
+}
