01fa3c463d267a1407cbca1b6c05e275e76ec409
FlowerFerry <flower_ferry@outlook.com>
2025-04-03 10:16:30 +0800

    -

--- include/mmbkpp/app/object_registrar.hpp
+++ include/mmbkpp/app/object_registrar.hpp
@@ -15,7 +15,7 @@ public:
     {
         object_factory<_Base>::instance().regi(_key, 
         [](void* _user_data) -> _Base* { return new _Derived(); },
-        [](base_type* _object, void* _user_data) { delete static_cast<_Derived*>(_object); }, 
+        [](_Base* _object, void* _user_data) { delete static_cast<_Derived*>(_object); }, 
         nullptr);
     }
 
